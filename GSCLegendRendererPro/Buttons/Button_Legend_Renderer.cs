225307270a3d4fa6d16d4bcf211d85e0ffc9a595
ghuotvez <gabriel.huot-vezina@nrcan-rncan.gc.ca>
2026-05-29 14:20:19 -0400

    Build new toolbar, buttons and icons set for ArcPro

---
+++ GSCLegendRendererPro/Buttons/Button_Legend_Renderer.cs
@@ -0,0 +1,31 @@
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.KnowledgeGraph;
+using ArcGIS.Desktop.Layouts;
+using ArcGIS.Desktop.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSCLegendRendererPro
+{
+    internal class Button_Legend_Renderer : Button
+    {
+        protected override void OnClick()
+        {
+            Form_Legend_Renderer legendForm = new Form_Legend_Renderer();
+            legendForm.ShowDialog();
+        }
+    }
+}
---
+++ GSCLegendRendererPro/ProWindows/Form_Legend_Renderer.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GSCLegendRendererPro
+{
+    /// <summary>
+    /// Interaction logic for Form_Legend_Renderer.xaml
+    /// </summary>
+    public partial class Form_Legend_Renderer : ArcGIS.Desktop.Framework.Controls.ProWindow
+    {
+        public Form_Legend_Renderer()
+        {
+            InitializeComponent();
+        }
+    }
+}
